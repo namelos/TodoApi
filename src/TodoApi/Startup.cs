7493ab03b7109d92f2b384adfc9b56429b95990b
Fred Wang <namelos@outlook.com>
2016-07-27 23:51:51 +0800

    register service

--- src/TodoApi/Startup.cs
+++ src/TodoApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -38,6 +39,8 @@ namespace TodoApi
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
