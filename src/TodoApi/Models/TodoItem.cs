0b5ef6d521dfafaa6c31a9a320584a405c468621
Fred Wang <namelos@outlook.com>
2016-07-27 23:51:38 +0800

    add todo model

--- src/TodoApi/Models/TodoItem.cs
+++ src/TodoApi/Models/TodoItem.cs
@@ -7,7 +7,7 @@ namespace TodoApi.Models
 {
     public class TodoItem
     {
-        public string key { get; set; }
+        public string Key { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
